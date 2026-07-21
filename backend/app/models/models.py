12e411bcd4ee5fec380982a2dd1271438c5e2416
ThomasM31 <Thomasmathiassen99@gmail.com>
2026-07-21 13:11:03 +0800

    commented out

--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -2,10 +2,11 @@ import datetime as dt
 import sqlalchemy as sql
 import db.database as db
 
+"""
 class Contact(db.Base):
-    """
-    Simple SQL for a contact
-    """
+    
+    #Simple SQL for a contact
+    
     __tablename__ = "contacts"
     id = sql.Column(sql.Integer, primary_key=True, index=True)
     first_name = sql.Column(sql.String, index=True)
@@ -14,3 +15,4 @@ class Contact(db.Base):
     phone_number = sql.Column(sql.String, index=True, unique=True)
     date_created = sql.Column(sql.DateTime, default=dt.datetime.now(dt.timezone.utc)) 
 
+"""
--- backend/app/services/services.py
+++ backend/app/services/services.py
@@ -1,6 +1,7 @@
 import db.database as _database
 import models.models as _models
 
+"""
 def add_tables():
     return _database.Base.metadata.create_all(bind=_database.engine)
-
+"""
