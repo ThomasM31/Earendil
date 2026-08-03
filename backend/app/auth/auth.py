7ea896edc3d3501899d6a7faf41285dd3b86bfe8
ThomasM31 <Thomasmathiassen99@gmail.com>
2026-08-03 17:33:43 +0800

    add verify_access_token()

--- backend/app/auth/auth.py
+++ backend/app/auth/auth.py
@@ -16,7 +16,7 @@ def hash_password(password:str) -> str:
 def verify_password(plain_password:str, hashed_password:str) -> bool:
     return password_hash.verify(plain_password, hashed_password)
 
-def create_access_token(data:dict, expires_delta: timedelta | None = None) -> str:
+def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
     """
         Creates a JWT access token
     """
@@ -28,9 +28,28 @@ def create_access_token(data:dict, expires_delta: timedelta | None = None) -> st
         expire = datetime.now(UTC) + timedelta(minutes=settings.access_token_expire_minutes)
 
     to_encode.update({"exp": expire})
+
     encoded_jwt = jwt.encode(
         to_encode,
         settings.secret_key.get_secret_value(),
         algorithm=settings.algorithm
     )
     return encoded_jwt
+
+def verify_access_token(token: str) -> str | None:
+    """
+        Verify a JWT access token and return the subject (user id) if valid
+    """
+    try:
+        payload = jwt.decode(
+            token,
+            settings.secret_key.get_secret_value(),
+            algorithms=[settings.algorithm],
+            options={"require":["exp", "sub"]}
+        )
+    except jwt.InvalidTokenError:
+        return None
+    
+    else:
+        return payload.get("sub")
+
