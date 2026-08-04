db2191fee453600d4b00adb55bd2e6b263d687d2
ThomasM31 <Thomasmathiassen99@gmail.com>
2026-08-04 13:04:21 +0800

    add user update schema

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -5,7 +5,7 @@ from typing import Optional
 # These are set on account creation, date_created will be generated
 class UserCreate(BaseModel):
     email: str = Field(max_length=120)
-    name: str = Field(max_length=120)
+    name: str = Field(max_length=70)
     username: str = Field(min_length=1, max_length=50)
     password: str = Field(min_length=8)
 
@@ -19,6 +19,10 @@ class UserCreate(BaseModel):
             }
         }
     )
+
+class UserUpdate(BaseModel):
+    username: str | None = Field(default=None, min_length=1, max_length=50)
+    email: str | None = Field(default=None, max_length=120)
 
 class UserLogin(BaseModel):
     username: str = Field(min_length=1, max_length=50)
