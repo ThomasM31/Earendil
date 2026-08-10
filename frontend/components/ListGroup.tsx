43109aaaade9a20de707e997585c3b39a3d28483
ThomasM31 <Thomasmathiassen99@gmail.com>
2026-08-10 14:41:30 +0800

    more react work

--- frontend/components/ListGroup.tsx
+++ frontend/components/ListGroup.tsx
@@ -1,22 +1,36 @@
 //import { Fragment } from "react";
+//import type { MouseEvent } from "react";
+
+import { useState } from "react";
 
 function ListGroup() {
-    const items = ["NY", "SF","Tokyo","London","Paris"];
+    let items = ["NY", "SF","Tokyo","London","Paris"];
+    
+    // Hook
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+
+    // Event handler
+    //const handleClick = (event: MouseEvent) => console.log(event);
 
     /**IF statement */
-    const getMessage = () => {items.length == 0 ? <p>No item found</p>: null}
+    const getMessage = () => {items.length == 0 ? <p>No items found</p>: null};
+
     return <>
             <h1>List</h1>
-            
             {getMessage}
             <ul className="list-group">
                 {items.map((item, index) => (
-                    <li className="list-group-item" 
-                        key={item} 
-                        onClick={() => console.log(item, index)} 
+                    <li 
+                     className={
+                        selectedIndex == index 
+                        ? "list-group-item active"
+                        : "list-group-item"} 
+                     key={item} 
+                     onClick={() => {setSelectedIndex(index);}} 
                     >
-                        {item}</li>
-                    ))};
+                        {item}
+                    </li>
+                ))};
             </ul>
         </>
 }
